b666c0ca083e7ef82833a1d6ae9796bbfa88c2b5
ponganan <joeyubu@gmail.com>
2022-08-21 07:23:32 +0700

    can't not run right now

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
             return ListView.builder(
               itemCount: snapshot.data!.docs.length,
               itemBuilder: (BuildContext context, int index) {
-                final docData = snapshot.data!.docs[index].data();
+                final docData = snapshot.data!.docs[index];
                 final dateTime = (docData['timestamp'] as Timestamp).toDate();
                 return ListTile(
                   title: Text(dateTime.toString()),
